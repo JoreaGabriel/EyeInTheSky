9acb8444ed7a52e0e10feb8989577686c3e98e53
Gabriel <Gabriel@DESKTOP-HBFJOI4>
2025-04-03 12:41:28 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EyeInTheSky
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
