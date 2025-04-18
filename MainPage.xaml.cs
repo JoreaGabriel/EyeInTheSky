7960be00d992dba41fe4dde31cdc63211c5b2806
Jorea Gabriel <joreagabriel@gmail.com>
2025-04-18 23:38:25 +0300

    Main page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,26 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            Shell.SetNavBarIsVisible(this, false);
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void btnSatImg_Clicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void btnMarsImg_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAPOD_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 
